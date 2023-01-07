809a102f60ce0a173df0aef634d0fa0e4e6efd29
v-Alqorith <vahapcandongel@hotmail.com>
2023-01-07 22:40:49 +0000

    commit

---
+++ linkedlist.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value) -> None:
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        newNode = Node(value)
+        if self.head is None:
+            self.head.next = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+        self.length += 1
+        return True
+
+    def pop(self):
+        temp = self.head
+        pre = self.head
+        if temp.next is not None:
+            temp = temp.next
+            pre = temp
+            self.tail = pre
+            self.tail.next = None
+            return temp
+
+    def prepend(self, value):
+        pass
+
+    def insert(self, index, value):
+        pass
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+
+my_ll = LinkedList(4)
+my_ll.append(5)
+my_ll.append(12)
+my_ll.pop()
+
+print(my_ll.tail.value)
